f168f370e909fd2be096a22cb88f6fdba93201f2
Наталија Поповић <natalijapopovic.rs@gmail.com>
2024-11-17 17:08:04 +0100

    Appearance for payment page created

--- psp-frontend/src/app/app-routing.module.ts
+++ psp-frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import { PaymentGatewayComponent } from './payment-gateway/payment-gateway.component';
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'pay.psp', component: PaymentGatewayComponent}
 ];
 
 @NgModule({
