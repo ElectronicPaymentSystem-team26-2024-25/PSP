b3aac1613b22406ce5721926dd5f47de40045269
IvanPartalo <ivanpartalo123@gmail.com>
2024-11-23 16:28:19 +0100

    psp gateway

---
+++ PSPGateway/src/main/java/com/example/PSPGateway/controller/GatewayController.java
@@ -0,0 +1,21 @@
+package com.example.PSPGateway.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("api/cardpayment")
+public class GatewayController {
+    @Autowired
+    RestTemplate restTemplate;
+    @PostMapping(consumes = "application/json", path = "/cardpaymentform/{bank}")
+    public ResponseEntity<Object> getCardPaymentRequestResponse(@RequestBody Object request, @PathVariable("bank") int bankPort)
+    {
+        String url = "http://localhost:"+bankPort+"/api/cardpayment/cardpaymentform";
+        ResponseEntity<Object> response = restTemplate.postForEntity(url, request, Object.class);
+        return new ResponseEntity<>(response.getBody(), HttpStatus.OK);
+    }
+}
---
+++ PSPGateway/src/test/java/com/example/PSPGateway/PspGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PSPGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PspGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
