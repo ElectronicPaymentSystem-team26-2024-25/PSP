7d1af5ce9030c37e7deb14b67b6bb9250f393d23
Наталија Поповић <natalijapopovic.rs@gmail.com>
2025-09-18 17:29:49 +0200

    Feat: admin can add new payment types on frontend

--- psp-frontend/src/app/payment-administration/payment-administration.component.ts
+++ psp-frontend/src/app/payment-administration/payment-administration.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { PaymentMethod, PaymentType } from '../model/subscription.model';
+import { PaymentMethod, PaymentType, StringToPaymentType } from '../model/subscription.model';
 import { AdministrationService } from '../service/administration.service';
 import { PaymentTypeToString } from '../model/subscription.model';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-payment-administration',
@@ -12,8 +13,15 @@ export class PaymentAdministrationComponent implements OnInit{
   
   methods: PaymentMethod[] = [];
 
-  showAssign: boolean = false;
+  showCreate: boolean = false;
   showEdit: boolean = false;
+
+  createForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      type: new FormControl('', [Validators.required]),
+      address: new FormControl('', [Validators.required]),
+      endpoint: new FormControl('', [Validators.required])
+    });
 
   constructor(private service: AdministrationService) {}
 
@@ -39,5 +47,39 @@ export class PaymentAdministrationComponent implements OnInit{
           alert(err);
         }
       })
+  }
+
+  showCreateModal(): void{
+    this.showCreate = true;
+  }
+
+  hideCreateModal(): void{
+    this.showCreate = false;
+  }
+
+  create(): void{
+    if(!this.createForm.valid){
+      alert('Check entered values. Somethings is not right.');
+      return;
+    }
+    else{
+      const desiredType = StringToPaymentType(this.createForm.value.type || "");
+      const paymentMethod: PaymentMethod = {
+        id: 0,
+        name: this.createForm.value.name || "",
+        type: desiredType,
+        active: true
+      }
+      this.service.createMethod(paymentMethod).subscribe({
+        next: (response: PaymentMethod) => {
+          this.methods.push(response);
+          this.hideCreateModal();
+        },
+        error: (err: any) => {
+          alert('Something went wrong. Try again later.');
+          this.hideCreateModal();
+        }
+      }) 
+    }
   }
 }
